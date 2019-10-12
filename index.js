6182aafa144bf8d3d3e6b08ff5dc4f99ca0fcf0d
Kevin Long <kevin.uu.long@gmail.com>
2019-10-11 20:54:42 -0400

    Added proper json loading
    NOTE: currently all levels are always loaded, this is SLOW, BAD, and should eventually be CHANGED

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
-const jetpack = require("fs-jetpack"),
+const jetpack = require("fs-jetpack"), //NOTE: remove if not used
       express = require("express"),
       app = express(),
       port = 3000,
-      bodyparser = require('body-parser')
-
+      requiredir = require('require-dir'),
+      levels = requiredir('./levels')
 
 //Allow loading of files from the public directory
-app.use(express.static('public'))
+app.use(express.static(__dirname+'public'))
+app.get("/json", (req, res) => {
+    res.json(levels);
+})
 //Load /public/index.html
 app.get('/', (req, res) => res.sendFile(__dirname+'/public/index.html'))
 //Log on successful launch
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-//jetpack.read('settings.json', 'json');
-//jetpack.write('settings.json', JSON.stringify(settings));
-
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({extended: true}));
-
-app.post('/profile', function (req, res, next) {
-    console.log(req.body.response)
-    //jetpack.write('test.json', JSON.stringify(req.body));
-    res.status(201).json(req.body.response);
-  })
