1c245ea46883c2d4e97bdb1a00430359fccbfc90
Kevin Long <kevin.uu.long@gmail.com>
2019-10-13 08:25:35 -0400

    Fixed level loading
    NOTE: Some changes wll likely still be needed. Also eventually should uninstall fs-jetpack

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const jetpack = require("fs-jetpack"), //NOTE: remove if not used
       express = require("express"),
       app = express(),
-      port = 3000,
+      port = process.env.PORT || 3000,
       requiredir = require('require-dir'),
-      levels = requiredir('./levels')
+      levels = requiredir('./levels'),
+      bp = require('body-parser')
 
+
+app.use(bp.json());
+app.use(bp.urlencoded({extended: true}));
 //Allow loading of files from the public directory
 app.use(express.static('public'));
 //Load /public/index.html
 app.get('/', (req, res) => res.sendFile(__dirname+'/public/index.html'))
-//Send levels as json
-app.get("/json", (req, res) => {
-    res.json(levels);
+//Send level as json
+app.post("/json", (req, res) => {
+    var level = req.body.level;
+    console.log(level+" requested");
+    res.json(levels[level]);
 })
 //Log on successful launch
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
